dea50e6159ba0d4e4c9b782d4140d376632dccb4
NikosZoros3533 <nikos7kozani@gmail.com>
2025-05-23 21:05:25 +0300

    reading GeoJson

--- src/Leaflet/MapComponent.jsx
+++ src/Leaflet/MapComponent.jsx
@@ -1,12 +1,13 @@
 import {
   MapContainer,
-  TileLayer,
+  TileLayer,GeoJSON
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
 import iconUrl from "leaflet/dist/images/marker-icon.png";
 import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+import points from "./../points.json";
 
 // Fix for default marker icons in Leaflet
 delete L.Icon.Default.prototype._getIconUrl;
@@ -16,24 +17,46 @@ L.Icon.Default.mergeOptions({
   shadowUrl: shadowUrl,
 });
 
-const center = [51.505, -0.09];
 
 
 
 const MapComponent = () => {
+  console.log(points);
+  function onEachCountry(feature, layer) {
+    layer.bindPopup(
+      `<div>
+        <h2>${feature.properties.name}</h2>
+        <div style="max-height:100px; overflow:auto;">
+          <p>${feature.properties.description}</p>
+        </div>
+      </div>`
+    );
+  }
+  
   return (
-    <div style={{ height: "70vh", width: "100%" }}>
+    <div style={{ height: "80vh", width: "100%" }}>
       <MapContainer
-        center={center}
-        zoom={20}
-        style={{ height: "100%", width: "100%" }}
+        center={[points.features[3].geometry.coordinates[1], points.features[0].geometry.coordinates[0]]}
+        zoom={12}
         scrollWheelZoom={true}
+        style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <GeoJSON
+          data={points}
+          style={() => ({
+            color: "blue",
+            weight: 2,
+            fillColor: "blue",
+            fillOpacity: 0.5,
+          })}
+          onEachFeature={onEachCountry}
         />
       </MapContainer>
+      
     </div>
   );
 };
