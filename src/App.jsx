3998c2569f0c160567684cdec7c8e49cd5b4a49b
NikosZoros3533 <nikos7kozani@gmail.com>
2025-05-23 19:48:13 +0300

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MapComponent from "./Leaflet/MapComponent";
+
+function App() {
+  return (
+    <>
+      <h1>Leaflet Learning</h1>
+      <MapComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Leaflet/MapComponent.jsx
@@ -0,0 +1,41 @@
+import {
+  MapContainer,
+  TileLayer,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+// Fix for default marker icons in Leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetinaUrl,
+  iconUrl: iconUrl,
+  shadowUrl: shadowUrl,
+});
+
+const center = [51.505, -0.09];
+
+
+
+const MapComponent = () => {
+  return (
+    <div style={{ height: "70vh", width: "100%" }}>
+      <MapContainer
+        center={center}
+        zoom={20}
+        style={{ height: "100%", width: "100%" }}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapComponent;
